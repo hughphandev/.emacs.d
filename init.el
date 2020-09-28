d96f509ec52a98e1abf359cf9d554b6c9e129251
Hugh Phan <hughphandev@gmail.com>
2020-09-29 06:47:10 +0700

    org init

--- init.el
+++ init.el
@@ -1,21 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key use-package try))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -26,16 +8,5 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-[
-(use-package try
-  :ensure t)
 
-(use-package which-key
-  :ensure t
-  :config (which-key-mode))
-
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-(defalias 'list-buffers 'ibuffer-other-window)
+(org-babel-load-file (expand-file-name "~/.emacs.d/JusaInit.org"))
