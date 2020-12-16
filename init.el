750b993763a5bb89ecd9122897a1b27465985b06
Hugh Phan <hughphandev@gmail.com>
2020-12-17 05:50:30 +0700

    clean up and fix some bu

--- init.el
+++ init.el
@@ -1,12 +1,1 @@
-(require 'package)
-(setq package-enable-at-startup nil)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			  ("elpa" . "https://elpa.gnu.org/packages/")
-			  ("org" . "https://orgmode.org/elpa/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
 (org-babel-load-file (expand-file-name "~/.emacs.d/JusaInit.org"))
